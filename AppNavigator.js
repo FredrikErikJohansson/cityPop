af3083b8506ab7f16f0c0f45bd68411d4726c261
FredrikErikJohansson <fredrik96.johansson@gmail.com>
2019-03-24 13:40:39 +0100

    Modified screen structure, added basic API functionality

--- AppNavigator.js
+++ AppNavigator.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from './screens/HomeScreen';
 import SearchScreen from './screens/SearchScreen';
-import CitiesScreen from './screens/CitiesScreen';
-import PopulationScreen from './screens/PopulationScreen';
 
 
 const AppNavigator = createStackNavigator({
@@ -12,12 +10,6 @@ const AppNavigator = createStackNavigator({
   },
   Search: {
     screen: SearchScreen,
-  },
-  Cities: {
-    screen: CitiesScreen,
-  },
-  Population: {
-    screen: PopulationScreen,
   },
 }, {
     initialRouteName: 'Home',
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,49 +1,85 @@
 import React from 'react';
-import { KeyboardAvoidingView, View, StyleSheet ,Text , TextInput, TouchableOpacity, Image} from 'react-native';
+import { KeyboardAvoidingView, View, StyleSheet ,Text , TextInput, TouchableOpacity, Image, ActivityIndicator, FlatList} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default class SearchScreen extends React.Component {
+  //TODO Redux
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {
+      text: '',
+      isLoading: true,
+      dataSource: null,
+    }
   }
 
   static navigationOptions = {
-    title: 'Search',
+    title: 'CityPop',
   };
 
   _onPressButtonSearch(title) {
-    if(title == 'CITY') {
-      this.props.navigation.navigate('Population');
-    }
-    else {
-      this.props.navigation.navigate('Cities');
-    }
+
+    //TODO: Add wrong input handler
+
+    const encodedValue = encodeURIComponent(this.state.text);
+    //fcode=PPLA == cities
+    //fcode=PPLC == capitals
+    let url = `http://api.geonames.org/searchJSON?q='${encodedValue}'&maxRows=10&username=weknowit&featureCode=PPLA&featureCode=PPLC`;
+
+    return fetch(url)
+        .then( (response) => response.json() )
+        .then( (responseJson) => {
+            this.setState({
+              isLoading: false,
+              dataSource: responseJson.geonames,
+            })
+        })
+
+      .catch((error) => {
+        console.log(error)
+      });
   }
 
   render() {
     const { navigation } = this.props;
     const title = navigation.getParam('title', 'DUMMY');
 
-    return (
-      <KeyboardAwareScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
-        <Text style={styles.header}>SEARCH BY {title}</Text>
-        <View style={styles.searchbarBorder}>
-          <TextInput
-            style={styles.searchbar}
-            maxLength={24}
-            placeholder={"Enter a " + title.toLowerCase() + "..."}
-            onChangeText={(text) => this.setState({text})}
+    if(this.state.isLoading)
+    {
+      return (
+        <KeyboardAwareScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
+          <Text style={styles.header}>SEARCH BY {title}</Text>
+          <View style={styles.searchbarBorder}>
+            <TextInput
+              style={styles.searchbar}
+              maxLength={24}
+              placeholder={"Enter a " + title.toLowerCase() + "..."}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+          </View>
+          <TouchableOpacity onPress={() => this._onPressButtonSearch(title)}>
+          <Image
+            style={{ width: 30, height: 30 }}
+            source={require('../assets/icon_search.png')}
           />
+          </TouchableOpacity>
+        </KeyboardAwareScrollView>
+      )
+    }
+    else {
+
+      let cities = this.state.dataSource.map((val, key) => {
+        return <View key={key}><Text>{val.name}, {val.population}</Text></View>
+      });
+
+      return (
+        <View style={styles.container}>
+          <Text style={styles.header}>{this.state.text}</Text>
+          {cities}
         </View>
-        <TouchableOpacity onPress={() => this._onPressButtonSearch(title)}>
-                  <Image
-                    style={{ width: 30, height: 30 }}
-                    source={require('../assets/icon_search.png')}
-                  />
-                </TouchableOpacity>
-      </KeyboardAwareScrollView>
-    );
+      );
+    }
   }
 }
 
@@ -72,6 +108,10 @@ const styles = StyleSheet.create({
   },
   searchbar: {
     fontSize: 16,
+    textAlign: 'center',
+    //color: '#004c40',
+  },
+  item: {
     textAlign: 'center',
     //color: '#004c40',
   },
